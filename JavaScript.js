315ccbbcb87125c3008a50c86b8d4f30b7a874fb
Laura Senko <laurasenko@gmail.com>
2017-07-26 00:18:33 -0400

    second commit

--- JavaScript.js
+++ JavaScript.js
@@ -18,54 +18,38 @@ var grocery = [
     price: 5,
     quantity: 2}
 ];
-
-  /*  function addItem(){
-        var $item = $("<li>");
-        var $input = $('#newItem');
-        $input.val(newItem.innerHTML);
-        $input.val(newItem.innerHTML);
-        var $listItems = $("#listItems");
-        $listItems.append($li);
-    }*/
-
+    //loops to print out what's in the array and add it to list
     for (var i = 0; i < grocery.length; i++)
     {
         var $item = $("<li>");
         $item.text("Item: " + grocery[i].name + "  Price: " + "$" + grocery[i].price.toFixed(2) + "  Quantity: " + grocery[i].quantity);
         $("#listItems").append($item);
     }
-
-     function totalBillNoTax () {
+    //for loop counts indexes in array, stores values into item variables, prints list items
+    //multiplies item prices by their quantities and adds items together for total bill
+    function totalBillNoTax () {
          var itemNsame;
          var itemPrice;
          var itemQuantity;
          var totalBillNoTax = 0;
-         //for loop counts indexes in array
-         // stores values into item variables
-         // prints list items
-         //multiplies item prices by their quantities and adds items together for total bill
+
          for (i = 0; i < grocery.length; i++)
          {
            totalBillNoTax += grocery[i].price * grocery[i].quantity;
-
-         //console.log( "Item: " + itemName + "  Price: " + "$" + itemPrice.toFixed(2) + "  Quantity: " + itemQuantity);}
-         //prints bill w/o tax and converts to 2 decimal points
-         //console.log ("Total bill without tax = $" + totalBillNoTax.toFixed(2));
           }
           return totalBillNoTax;  //return this into variable that can be accessed in totalBill function
-      }
+    }
      //calculates tax
-     function calcTax(subtotal) {
+    function calcTax(subtotal) {
           return subtotal * 0.06;
-     }
-$('button').on('click', function()
-{
-     //function totalBill() {
+    }
+    //prints what we returned from totalBillNoTax function + calcTax function and rounds to 2 places
+    $('button').on('click', function()
+    {
           var noTax = totalBillNoTax();
-          var total = noTax + calcTax(noTax); /*totalBillNoTax();*/
+          var total = noTax + calcTax(noTax);
           var $target = $("#total");
           $target.text(total.toFixed(2));
-    //  }
-});
+    });
 
 });
